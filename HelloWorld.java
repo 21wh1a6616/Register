bf40511b114f7c64d81e783cd16c708ff3e6fcdf
21wh1a6616 <96616246+21wh1a6616@users.noreply.github.com>
2024-07-31 11:25:40 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ class Hello
 {
     public static void main(String []args)
     {
-        System.out.println("Hello World Java Program new");
+        System.out.println("Hello World - Java Program");
     }
 };
